3d0536c658bb225fcace4d21b9ceeb9c3f6df48e
ayoadeadedamola198@gmail.com <ayoadeadedamola198@gmail.com>
2023-07-27 18:35:40 +0100

    power

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 def pow(a, b):
-    return a ^ b
+        result = 1
+    
+        # If b is negative, invert a and make b positive
+        if b < 0:
+            a = 1 / a
+            b = -b
+    
+        # Calculate a^b by multiplying 'a' 'b' times
+        for _ in range(b):
+            result *= a
+    
+        return result
