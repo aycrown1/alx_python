b31c15ad2be088659c91843c3f1164ce5df5cff1
ayoadeadedamola198@gmail.com <ayoadeadedamola198@gmail.com>
2023-08-21 14:26:35 +0100

    best score

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+
     max_score = float('-inf')
     max_key = None
     for key, value in a_dictionary.items():
